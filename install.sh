508dee17cfd7d80bdf43b147d5262a64834d6d35
Ivan Antolic-Soban <ivan.soban@gmail.com>
2014-04-24 19:21:56 -0400

    Fixing linking recursion problems and PWD cross platform issues.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp -RLfv ~/.bash_profile ~/dotfiles_backup
 cp -RLfv ~/.bashrc ~/dotfiles_backup
 cp -RLfv ~/.git-autocompletion.sh ~/dotfiles_backup
 cp -RLfv ~/.gitattributes ~/dotfiles_backup
-cp -Rlfv ~/.gitconfig ~/dotfiles_backup
+cp -RLfv ~/.gitconfig ~/dotfiles_backup
 cp -RLfv ~/.gitignore ~/dotfiles_backup
 cp -RLfv ~/.screenrc ~/dotfiles_backup
 cp -RLfv ~/.ssh/config ~/dotfiles_backup
@@ -19,15 +19,15 @@ cp -RLfv ~/.vimrc ~/dotfiles_backup
 echo
 echo "[INFO] Symlinking environment"
 
-ln -fsv $(PWD)/bash/bash_profile ~/.bash_profile
-ln -fsv $(PWD)/bash/bashrc ~/.bashrc
-ln -fsv $(PWD)/git/git-autocompletion.sh ~/.git-autocompletion.sh
-ln -fsv $(PWD)/git/gitattributes ~/.gitattributes
-ln -fsv $(PWD)/git/gitconfig ~/.gitconfig
-ln -fsv $(PWD)/git/gitignore ~/.gitignore
-ln -fsv $(PWD)/screen/screenrc ~/.screenrc
-ln -fsv $(PWD)/ssh/config ~/.ssh/config
-ln -fsv $(PWD)/bash/inputrc ~/.inputrc
+ln -fsv $(pwd)/bash/bash_profile ~/.bash_profile
+ln -fsv $(pwd)/bash/bashrc ~/.bashrc
+ln -fsv $(pwd)/git/git-autocompletion.sh ~/.git-autocompletion.sh
+ln -fsv $(pwd)/git/gitattributes ~/.gitattributes
+ln -fsv $(pwd)/git/gitconfig ~/.gitconfig
+ln -fsv $(pwd)/git/gitignore ~/.gitignore
+ln -fsv $(pwd)/screen/screenrc ~/.screenrc
+ln -fsv $(pwd)/ssh/config ~/.ssh/config
+ln -fsv $(pwd)/bash/inputrc ~/.inputrc
 
 echo
 echo "[INFO] Installing Other Packages"
@@ -35,13 +35,13 @@ echo "[INFO] Installing Other Packages"
 echo
 echo "[INFO] Installing oh-my-zsh"
 
-ln -Fsv $(PWD)/oh-my-zsh ~/.oh-my-zsh
-ln -Fsv $(PWD)/zsh/zshrc ~/.zshrc
+ln -Fsv $(pwd)/oh-my-zsh/ ~/.oh-my-zsh
+ln -Fsv $(pwd)/zsh/zshrc ~/.zshrc
 chsh -s /bin/zsh
 
 echo
 echo "[INFO] Installing vim and extras"
 
-ln -fsv $(PWD)/vim/vimrc ~/.vimrc
-ln -Fsv $(PWD)/vim ~/.vim
+ln -Fsv $(pwd)/vim/ ~/.vim
+ln -fsv ~/.vim/vimrc ~/.vimrc
 vim +PluginInstall +qall
